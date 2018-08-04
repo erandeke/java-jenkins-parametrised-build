d237eeb4807863baf2cbdfbcbea69c3d4e294cec
unknown <erande.kedar@gmail.com>
2018-08-04 23:47:07 +0530

    git commit

---
+++ src/main/java/ParametrJob.java
@@ -0,0 +1,13 @@
+public class ParametrJob {
+
+    public static void main(String args[])
+    {
+        /*  String values=System.getenv("Trade_Selection");
+          System.out.println(values);*/
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("Kedar");
+        }
+
+
+    }}
