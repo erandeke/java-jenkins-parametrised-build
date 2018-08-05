aae450abe283608588fc0468ecc64eb510eb80d0
Kedar Erande <erande.kedar@gmail.com>
2018-08-05 11:50:45 +0530

    Add files via upload

---
+++ src/main/java/TestJava.java
@@ -0,0 +1,14 @@
+package com.jenkins.parameter;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		String val=System.getProperty(args[0]);
+		System.out.println(val);
+	}
+
+}
